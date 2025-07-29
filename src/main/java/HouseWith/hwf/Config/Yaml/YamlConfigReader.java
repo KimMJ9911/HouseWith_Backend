57dba01859583f801489dfcd3a1e02b89e9686f5
Trifolium991119 <cvzx991119@gmail.com>
2025-07-29 20:26:43 +0900

    new

--- src/main/java/HouseWith/hwf/Config/Yaml/YamlConfigReader.java
+++ src/main/java/HouseWith/hwf/Config/Yaml/YamlConfigReader.java
@@ -1,12 +1,12 @@
-package HouseWith.hwf.Config.Yaml;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class YamlConfigReader {
-    @Bean
-    public void yml_setting() {
-
-    }
-}
+//package HouseWith.hwf.Config.Yaml;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class YamlConfigReader {
+//    @Bean
+//    public void yml_setting() {
+//
+//    }
+//}
