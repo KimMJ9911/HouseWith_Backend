96473bf98bd4fd0ac63535c0a650cee5d8081802
Trifolium991119 <cvzx991119@gmail.com>
2025-07-29 20:38:48 +0900

    오류 수정

--- src/main/java/HouseWith/hwf/web/Login/Info/InitController/InitController.java
+++ src/main/java/HouseWith/hwf/web/Login/Info/InitController/InitController.java
@@ -23,34 +23,6 @@ public class InitController {
     private final InfoService infoService;
     private final JwtUtil jwtUtil;
 
-    @Value("${univcert.key}")
-    private String UNIV_CERT_KEY;
-
-    /**
-     * @param YonseiEmail : 학교 인증에 필요한 이메일
-     * @return : 반환값은 학교 인증 여부 + 학교 재학 여부 모두 반환
-     *
-     * 논점 - 학교 인증이 꼭 필요한지???
-     */
-    //@PostMapping("univAuth")
-    public ResponseEntity<?> UnivCertification_email(
-            @RequestParam String YonseiEmail ,
-            @RequestParam Long memberId) throws IOException {
-//        Map<String , Object> cert_values = UnivCert.certify(
-//                UNIV_CERT_KEY ,
-//                YonseiEmail ,
-//                "연세대학교 미래캠퍼스" ,
-//                true
-//                );
-        Member member = memberRepository.findByMemberId(memberId);
-//        boolean cert_check = (boolean) cert_values.get("success");
-        //인증 완료한 이메일인 경우 저장
-
-//        memberRepository.save(initService.authUniv(YonseiEmail));
-
-
-        return ResponseEntity.ok("인증코드 전송");
-    }
 
     /**
      * 6/27 - 생성
